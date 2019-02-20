015355ae17665c722bff5be7c9bae3f760397ad3
jack <47188744+jackgalipeault@users.noreply.github.com>
2019-02-20 16:44:41 -0500

    data model setup

---
+++ js/main.js
@@ -0,0 +1,67 @@
+let mealEntries = [
+{
+    main: `Chicken Breast`,
+    side: `Brown Rice`,
+    type: `Dinner`,
+    drink: `Water`,
+    cal: 600,
+    datetime: {
+        time: `7:15PM`,
+        month: `February`,
+        day: 18,
+        year: 2019,
+    },
+},
+{
+    main: null,
+    side: null,
+    type: `Drink`,
+    drink: `Espresso`,
+    cal: 50,
+    datetime: {
+        time: `6:25 PM`,
+        month: `February`,
+        day: 18,
+        year: 2019,
+    },
+},
+{
+    main: `Pretzels`,
+    side: null,
+    type: `Snack`,
+    drink: `Water`,
+    cal: 250,
+    datetime: {
+        time: `4:30PM`,
+        month: `February`,
+        day: 18,
+        year: 2019,
+    },
+},
+{
+    main: `BLT Sandwich`,
+    side: `Fries`,
+    type: `Lunch`,
+    drink: `Soda`,
+    cal: 250,
+    datetime: {
+        time: `1:05PM`,
+        month: `February`,
+        day: 18,
+        year: 2019,
+    },
+},
+{
+    main: `Greek Yogurt`,
+    side: `Granola`,
+    type: `Breakfast`,
+    drink: `Coffee`,
+    cal: 500,
+    datetime: {
+        time: `9:15AM`,
+        month: `February`,
+        day: 18,
+        year: 2019,
+    },
+}
+];
