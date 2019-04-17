7c626189b570d526fc0a9dec4a468943aa5bf635
jack <47188744+jackgalipeault@users.noreply.github.com>
2019-04-17 09:49:19 -0400

    video folder, js output work, css fixes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 let mealEntries = [
 {
     main: `Chicken Breast`,
@@ -10,72 +11,52 @@ let mealEntries = [
         day: 18,
         year: 2019,
     },
-},
-{
-    main: null,
-    side: null,
-    type: `Drink`,
-    drink: `Espresso`,
-    cal: 50,
-    date: {
-        month: `February`,
-        day: 18,
-        year: 2019,
-    },
-},
-{
-    main: `Pretzels`,
-    side: null,
-    type: `Snack`,
-    drink: `Water`,
-    cal: 250,
-    date: {
-        month: `February`,
-        day: 18,
-        year: 2019,
-    },
-},
-{
-    main: `BLT Sandwich`,
-    side: `Fries`,
-    type: `Lunch`,
-    drink: `Soda`,
-    cal: 750,
-    date: {
-        month: `February`,
-        day: 18,
-        year: 2019,
-    },
-},
-{
-    main: `Greek Yogurt`,
-    side: `Granola`,
-    type: `Breakfast`,
-    drink: `Coffee`,
-    cal: 500,
-    date: {
-        month: `February`,
-        day: 18,
-        year: 2019,
-    },
-}
-];
-
-let listMeals = [
-    {
-        id: 0,
-        main: `Greek Yogurt`,
-        side: `Granola`,
-        type: `Breakfast`,
-        drink: `Coffee`,
-        cal: 500,
-        date: {
-            month: `February`,
-            day: 18,
-            year: 2019,
-        },
-    }
-];
-
+}];
+*/
 const allMeals = document.getElementById('allMeals');
 const mealEntry = document.getElementById('mealEntry')
+
+mealEntry.addEventListener('addButton', event => {
+    mealEntries.push({ 
+    id: mealEntries.length,
+    main: mealMain.value,
+    side: sideDish.value,
+    type: mealType.value,
+    drink: mealDrink.value,
+    cal: mealCal.value,
+    date: {
+        month: dateMonth.value,
+        day: dateDay.value,
+        year: dateYear.value,
+    },
+ });
+
+   // Clear out the input field so we can use it again
+    mealMain.value = '',
+    sideDish.value = '',
+    mealType.value = '',
+    mealDrink.value = '', 
+    mealCal.value = '',
+    dateMonth.value = '',
+    dateDay.value = '',
+    dateYear.value = '',
+
+   // Reprint the new list
+   updateList();
+ 
+   // prevent the submit from leaving the page
+   event.preventDefault();
+
+   // OUTPUT THE RESULTS
+function updateList() {
+    // "map()" runs once for each Object in the Array
+    allMeals.innerHTML = mealEntries.map(mealListItem => 
+        '<li>${ mealMain.value } – February 18 2019<br><strong>Chicken Breast, Brown Rice, Water</strong><br>600 Calories<br><br></li>' ).join('');
+
+    // Note we used the shortcut "ternary" operator to add "if" conditions into the output above.
+    // Ternary format:
+    //    ( trueOrFalseVariableOrCondition ) ? 'condition was true!' : 'condition was false!'
+    // These can go right into our template evaluation blocks: ${ }
+  }
+
+});
